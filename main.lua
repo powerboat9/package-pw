eaa55a9999651dbb0f7887bf85e8d0bcad6d6f4d
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-22 22:46:59 -0400

    Did some work

--- main.lua
+++ main.lua
@@ -1,5 +1,9 @@
 local tArgs = {...}
 local dir = assert(tArgs[1] and fs.isDir(dir), "Please Specify A Directory")
+if dir:sub(-1, -1) == "/" then
+    dir = dir:sub(1, -2)
+end
+local preventHTTP = tArgs[2] or ((not http) and (function() printError("[WARN] HTTP API unavailable"); return true end))
 
 function getName(s)
     return s:sub(s:find("[^/]*$"))
@@ -18,15 +22,23 @@ function escape(s)
 end
 
 local fileFormat = "do\n    local file = fs.open(%s, \"w\")\n    file.write(%s)\n    file.close()\nend\n"
---local httpFileFormat = "do\n    local file = fs.open(%s, \"w\")\n    local handle = http.get(%s)\n    local data = handle.readAll()\n    handle.close()\n    file.write(data)\n    file.close()\nend\n"
+local httpFileFormat = "do\n    local file = fs.open(%s, \"w\")\n    local handle = http.get(%s)\n    local data = handle.readAll()\n    handle.close()\n    file.write(data)\n    file.close()\nend\n"
 
 function appendFile(file)
     local fileHandle = fs.open(file, "r")
-    saveFile = saveFile .. fileFormat:format("extract .. \"/\" .. " .. getName(file), fileHandle.readAll())
+    saveFile = saveFile .. fileFormat:format("extract .. \"/\" .. " .. getName(file), escape(fileHandle.readAll()))
     fileHandle.close()
 end
 
-function appendURL(url, name)
+function appendURL(url, file)
+    if preventHTTP then
+        saveFile = saveFile .. httpFileFormat:format(file:sub(#dir + 2, -1), url)
+    else
+        local handle = http.get(url)
+        saveFile = saveFile .. fileFormat:format("extract .. \"/\" .. " .. getName(file), escape(handle.readAll()))
+        handle.close()
+    end
+end
 
 for k, v in ipairs(fs.list(dir)) do
     
